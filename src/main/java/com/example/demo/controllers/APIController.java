b71ea5b406f932ef91be8016a46e9d12f93515af
Nitish Gadangi <nitishgadangi@gmail.com>
2021-12-07 19:54:27 +0530

    add dummy chat history api

--- src/main/java/com/example/demo/controllers/APIController.java
+++ src/main/java/com/example/demo/controllers/APIController.java
@@ -1,11 +1,12 @@
 package com.example.demo.controllers;
 
-import com.example.demo.models.SignIn;
-import com.example.demo.models.SignUp;
-import com.example.demo.models.Status;
+import com.example.demo.models.*;
 import com.example.demo.service.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class APIController {
@@ -33,5 +34,16 @@ public class APIController {
     @RequestMapping("/")
     public String homePage(){
         return "POST requests only";
+    }
+
+    @RequestMapping("/chat/history")
+    public ChatHistory getChatHistory(){
+        ArrayList<Chat> chatArrayList = new ArrayList<>();
+        chatArrayList.add(new Chat("Chat1", "https://banner2.cleanpng.com/20180427/ubq/kisspng-royalty-free-computer-icons-stock-photography-5ae31695b3df41.9958176715248318937368.jpg","1638886816","Hello World", ""));
+        chatArrayList.add(new Chat("Chat2", "https://banner2.cleanpng.com/20180427/ubq/kisspng-royalty-free-computer-icons-stock-photography-5ae31695b3df41.9958176715248318937368.jpg","1638886816","Hello World", ""));
+        chatArrayList.add(new Chat("Chat3", "https://banner2.cleanpng.com/20180427/ubq/kisspng-royalty-free-computer-icons-stock-photography-5ae31695b3df41.9958176715248318937368.jpg","1638886816","Hello World", ""));
+        chatArrayList.add(new Chat("Chat4", "https://banner2.cleanpng.com/20180427/ubq/kisspng-royalty-free-computer-icons-stock-photography-5ae31695b3df41.9958176715248318937368.jpg","1638886816","Hello World", ""));
+        ChatHistory history = new ChatHistory(true, "success", chatArrayList);
+        return history;
     }
 }
